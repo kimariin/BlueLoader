70b87f8880526fd746de65dd02905ade5ffdd262
Kimari <kimariin@proton.me>
2025-08-07 18:44:47 -0400

    Add filesystem dumper to payload
    Only grabs app0 (the Blu-ray player's application directory) because
    that seems to be the only interesting place we're allowed to see without
    a kernel exploit.
    
    Updated topsecret README with instructions on how to use the payload to
    solve the bdjstack/rt problem.

--- src/org/bdj/payload/Payload.java
+++ src/org/bdj/payload/Payload.java
@@ -1,5 +1,13 @@
 package org.bdj.payload;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.bdj.UITextConsole;
 import org.bdj.payload.LibKernel.KernelModuleInfo;
 
@@ -22,5 +30,70 @@ public class Payload {
 
 		// Example for non-working syscall:
 		console.add("getpgid: " + lk.syscall(LibKernel.SYS_getpgid));
+
+		// If someone connects to port 9030, dump the entire accessible filesystem to a zip file
+		ServerSocket server = new ServerSocket(9030);
+		console.add("ZIP dumper listening on port 9030");
+		boolean dumped = false;
+		while (!dumped) {
+			Socket client = server.accept();
+			String clientAddr = client.getInetAddress().getHostAddress();
+			int clientPort = client.getPort();
+			console.add("ZIP dumper client connected: " + clientAddr + ":" + clientPort);
+
+			OutputStream output = client.getOutputStream();
+			ZipOutputStream zip = new ZipOutputStream(output);
+
+			File app0 = new File("/app0");
+			Payload.dumpDirectoryToZip(zip, app0, console);
+
+			// Just class files, e.g. /dsm/app_base/org/bdj/MainXlet.class
+			// File cwd = new File(".");
+			// Payload.dumpDirectoryToZip(zip, cwd, console);
+
+			// Just to show where the Blu-ray is mounted
+			// File disc = new File("/disc");
+			// Payload.dumpDirectoryToZip(zip, disc, console);
+
+			zip.finish();
+			zip.close();
+			client.close();
+			dumped = true;
+		}
+		server.close();
+	}
+
+	private static void dumpDirectoryToZip(ZipOutputStream zip, File dir, UITextConsole console) {
+		if (!dir.isDirectory()) {
+			console.add(dir.getAbsolutePath() + " is not a directory");
+			return;
+		}
+		File[] fs = dir.listFiles();
+		for (int i = 0; i < fs.length; i++) {
+			File f = fs[i];
+			if (f.isDirectory()) {
+				dumpDirectoryToZip(zip, f, console);
+			} else if (f.canRead()) {
+				try {
+					// I assume the leading / is a bad idea?
+					console.add("Dumping: " + f.getAbsolutePath());
+					ZipEntry e = new ZipEntry(f.getAbsolutePath().substring(1));
+					zip.putNextEntry(e);
+					FileInputStream fi = new FileInputStream(f);
+					while (fi.available() > 0) {
+						byte[] bytes = new byte[fi.available()];
+						fi.read(bytes);
+						zip.write(bytes);
+					}
+					fi.close();
+					zip.closeEntry();
+				} catch (Throwable e) {
+					console.add("Error while reading " + f.getAbsolutePath());
+					console.add(e);
+				}
+			} else {
+				console.add("Not allowed to read " + f.getAbsolutePath());
+			}
+		}
 	}
 }
--- thirdparty/topsecret/README.txt
+++ thirdparty/topsecret/README.txt
@@ -1,18 +1,27 @@
 These files must be retrieved from your PS4 and placed here:
 
-/app/NPXS20113/bdjstack/bdjstack.jar
-/app/NPXS20113/bdjstack/lib/rt.jar
+	/app/NPXS20113/bdjstack/bdjstack.jar
+	/app/NPXS20113/bdjstack/lib/rt.jar
 
-If you don't have a jailbroken PS4, there's a roundabout way to get them. You can grab a decrypted
-firmware 7.00 package from https://darthsternie.net/ps4-decrypted-firmwares/ and extract them from
-PS4UPDATE2.PUP.dec using ps4-pup-unpacker:
+The easiest way to do it is to use the example payload, which you should be able to get from the
+project's GitHub releases page. Once you've launched BlueLoader on your PS4, just do:
 
-sudo apt install cmake
-git clone --recurse-submodules https://github.com/Zer0xFF/ps4-pup-unpacker
-cmake -Bps4-pup-unpacker -Sps4-pup-unpacker
-cmake --build ps4-pup-unpacker
-ps4-pup-unpacker/pup_unpacker PS4UPDATE2.PUP.dec
-sudo mkdir -p /mnt/ps4 && sudo mount PS4UPDATE2/system_ex_fs_image.img /mnt/ps4
-cp /mnt/ps4/app/NPXS20113/bdjstack/bdjstack.jar thirdparty/topsecret/
-cp /mnt/ps4/app/NPXS20113/bdjstack/lib/rt.jar thirdparty/topsecret/
-sudo umount /mnt/ps4 && sudo rmdir /mnt/ps4
+	cat payload.jar | netcat 192.168.x.x 9025 -q0
+	netcat 192.168.x.x 9030 > app0.zip
+	unzip app0.zip
+	cp app0/bdjstack/bdjstack.jar thirdparty/topsecret/
+	cp app0/bdjstack/lib/rt.jar   thirdparty/topsecret/
+	rm -r app0 app0.zip
+
+Alternatively, there's a roundabout way to get them. You can grab a decrypted firmware 7.00 package
+from https://darthsternie.net/ps4-decrypted-firmwares/ and extract it using ps4-pup-unpacker:
+
+	sudo apt install cmake
+	git clone --recurse-submodules https://github.com/Zer0xFF/ps4-pup-unpacker
+	cmake -Bps4-pup-unpacker -Sps4-pup-unpacker
+	cmake --build ps4-pup-unpacker
+	ps4-pup-unpacker/pup_unpacker PS4UPDATE2.PUP.dec
+	sudo mkdir -p /mnt/ps4 && sudo mount PS4UPDATE2/system_ex_fs_image.img /mnt/ps4
+	cp /mnt/ps4/app/NPXS20113/bdjstack/bdjstack.jar thirdparty/topsecret/
+	cp /mnt/ps4/app/NPXS20113/bdjstack/lib/rt.jar   thirdparty/topsecret/
+	sudo umount /mnt/ps4 && sudo rmdir /mnt/ps4
